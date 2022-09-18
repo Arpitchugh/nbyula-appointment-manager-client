4207390404c2be6f98b7ae3e49b0a4a3d91bc1d3
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2022-09-18 20:25:39 +0530

    feat: :sparkles: list all events on calender

--- src/action/event.action.js
+++ src/action/event.action.js
@@ -1,7 +1,21 @@
+import { eventsActions } from '../slices/event.slice';
 import api from '../util/api.util';
 
 export function createEvent(body) {
 	return async () => {
 		return await api.post('/events', body);
+	};
+}
+
+export function getUserEvents() {
+	return async dispatch => {
+		const res = await api.get('/events');
+		const formattedEvents = res.records.map(event => ({
+			...event,
+			start: new Date(event.start),
+			end: new Date(event.end),
+		}));
+		dispatch(eventsActions.replaceUserEvents(formattedEvents));
+		return res;
 	};
 }
--- src/pages/Home/components/calendar/index.jsx
+++ src/pages/Home/components/calendar/index.jsx
@@ -1,16 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
 import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
 import moment from 'moment';
 import CreateEventModal from '../createEventModal';
 import './index.less';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserEvents } from '../../../../action/event.action';
 
 function AwesomeCalendar() {
+	const dispatch = useDispatch();
+	const { userEvents } = useSelector(state => state.events);
 	const localizer = momentLocalizer(moment);
 	const DnDCalendar = withDragAndDrop(Calendar);
 	const [isCreateEventModalVisible, setIsCreateEventModalVisible] =
 		useState(false);
 	const [createdCalendarEvent, setCreatedCalendarEvent] = useState({});
+
+	useEffect(() => {
+		dispatch(getUserEvents());
+	}, []);
 
 	const selectSlotHandler = event => {
 		setCreatedCalendarEvent(event);
@@ -29,7 +37,7 @@ function AwesomeCalendar() {
 					min={new Date(1970, 1, 1, 0)}
 					scrollToTime={new Date(1970, 1, 1, 3)}
 					localizer={localizer}
-					// events={events}
+					events={userEvents}
 					defaultView={Views.WEEK}
 					onSelectSlot={selectSlotHandler}
 					selectable

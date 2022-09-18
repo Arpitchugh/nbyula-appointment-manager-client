f04ced9eaa90167a2811bee4a7e2c3dc2bd91c6c
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2022-09-18 12:26:15 +0530

    feat: :sparkles: modal for adding details for event

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,41 +1,15 @@
-import React, { useCallback, useState } from 'react';
-import { Button, Typography } from 'antd';
-import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
-import withDragAndDrop from 'react-big-calendar/lib/addons/dragAndDrop';
-import moment from 'moment';
-import './index.less';
+import React from 'react';
+import { Typography } from 'antd';
+import AwesomeCalendar from './components/calendar';
 
 const { Title } = Typography;
 const Home = () => {
-	const localizer = momentLocalizer(moment);
-	const DnDCalendar = withDragAndDrop(Calendar);
-	const [events, setEvents] = useState([]);
-
-	console.log(events);
-
-	const selectSlotHandler = event => {
-		setEvents(prevState => [
-			...prevState,
-			{ start: event.start, end: event.end },
-		]);
-	};
-
 	return (
 		<>
-			<section>
+			<main>
 				<Title level={2}>Hi terraformer!!!</Title>
-				<div className='h-screen'>
-					<DnDCalendar
-						min={new Date(1970, 1, 1, 0)}
-						scrollToTime={new Date(1970, 1, 1, 3)}
-						localizer={localizer}
-						events={events}
-						defaultView={Views.WEEK}
-						onSelectSlot={selectSlotHandler}
-						selectable
-					/>
-				</div>
-			</section>
+				<AwesomeCalendar />
+			</main>
 		</>
 	);
 };

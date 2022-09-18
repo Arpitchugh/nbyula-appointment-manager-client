dd839b44c6239a71e81d9d77e50782ec5d7246e5
Arpit Chugh <arpitchugh.jbd.ac@gmail.com>
2022-09-19 00:39:38 +0530

    feat: :sparkles: mark as available button in view event modal

--- src/pages/Home/components/calendar/components/viewEventModal/index.jsx
+++ src/pages/Home/components/calendar/components/viewEventModal/index.jsx
@@ -6,14 +6,18 @@ import { deleteEvent } from '../../../../../../action/event.action';
 function ViewEvent({ visible, setVisible, event }) {
 	const dispatch = useDispatch();
 	const [loading, setLoading] = useState(false);
+
 	const deleteEventHandler = async () => {
-		await dispatch(deleteEvent(event._id))
-			.then(() => message.success('event deleted successfully'))
-			.finally(() => {
-				setLoading(false);
-				setVisible(false);
-			});
+		try {
+			setLoading(true);	
+			await dispatch(deleteEvent(event._id));
+			message.success('event deleted successfully');
+			setVisible(false);
+		} finally {
+			setLoading(false);
+		}
 	};
+
 	return (
 		<Modal
 			open={visible}
@@ -22,60 +26,65 @@ function ViewEvent({ visible, setVisible, event }) {
 			onCancel={() => setVisible(false)}
 			footer={null}
 		>
-			<div className='flex items-center mb-3'>
-				<Typography.Title level={4} className='!m-0'>
-					Title:
-				</Typography.Title>
-				<Typography.Title level={5} className='!m-0 !ml-3'>
-					{event.title}
-				</Typography.Title>
-			</div>
+			{event.type === 'appointment' ? (
+				<>
+					<div className='flex items-center mb-3'>
+						<Typography.Title level={4} className='!m-0'>
+							Title:
+						</Typography.Title>
+						<Typography.Title level={5} className='!m-0 !ml-3'>
+							{event.title}
+						</Typography.Title>
+					</div>
 
-			<div className='flex items-center mb-3'>
-				<Typography.Title level={4} className='!m-0'>
-					Agenda:
-				</Typography.Title>
-				<Typography.Paragraph className='!m-0 !ml-3'>
-					{event.agenda}
-				</Typography.Paragraph>
-			</div>
+					<div className='flex items-center mb-3'>
+						<Typography.Title level={4} className='!m-0'>
+							Agenda:
+						</Typography.Title>
+						<Typography.Paragraph className='!m-0 !ml-3'>
+							{event.agenda}
+						</Typography.Paragraph>
+					</div>
 
-			<div className='flex items-center mb-3'>
-				<Typography.Title level={4} className='!m-0'>
-					Organizer:
-				</Typography.Title>
-				<Typography.Paragraph className='!m-0 !ml-3'>
-					{event?.organizer?.name}
-				</Typography.Paragraph>
-			</div>
+					<div className='flex items-center mb-3'>
+						<Typography.Title level={4} className='!m-0'>
+							Organizer:
+						</Typography.Title>
+						<Typography.Paragraph className='!m-0 !ml-3'>
+							{event?.organizer?.name}
+						</Typography.Paragraph>
+					</div>
 
-			<div className='flex flex-col  justify-center mb-3'>
-				<Typography.Title level={4} className='!m-0'>
-					Guests:
-				</Typography.Title>
-				<List
-					className='!m-0 !ml-3'
-					dataSource={event.guests}
-					renderItem={(guest, i) => (
-						<List.Item>
-							<Typography.Text>
-								{`${i + 1})`} {guest?.name} {`<${guest?.email}>`}
-							</Typography.Text>
-						</List.Item>
-					)}
-				/>
-			</div>
-			<Button
-				danger
-				loading={loading}
-				type='ghost'
-				onClick={() => {
-					setLoading(true);
-					deleteEventHandler();
-				}}
-			>
-				delete event
-			</Button>
+					<div className='flex flex-col  justify-center mb-3'>
+						<Typography.Title level={4} className='!m-0'>
+							Guests:
+						</Typography.Title>
+						<List
+							className='!m-0 !ml-3'
+							dataSource={event.guests}
+							renderItem={(guest, i) => (
+								<List.Item>
+									<Typography.Text>
+										{`${i + 1})`} {guest?.name} {`<${guest?.email}>`}
+									</Typography.Text>
+								</List.Item>
+							)}
+						/>
+					</div>
+					<Button
+						danger
+						loading={loading}
+						type='ghost'
+						onClick={deleteEventHandler}
+					>
+						delete event
+					</Button>
+				</>
+			) : (
+				<Button type='primary' onClick={deleteEventHandler} loading={loading}>
+					Mark as Available
+				</Button>
+			)}
 		</Modal>
 	);
 }
